b0e409ac0be1e4b7ead8ed8b216199732070e22e
Anna Atiskova <anna.atiskova@gmail.com>
2023-01-07 01:59:21 +0200

    task1

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,31 @@
 // Add imports above this line
+
 import { galleryItems } from './gallery-items';
+import SimpleLightbox from "simplelightbox"; 
+import "simplelightbox/dist/simple-lightbox.min.css";
+
 // Change code below this line
 
 console.log(galleryItems);
+
+const galleryList = document.querySelector(".gallery");
+
+const makeGalleryCard = ({ preview, original, description } = {}) => {
+  return `
+  <a class="gallery__item" href="${original}">
+  <img class="gallery__image" src="${preview}" alt="${description}" />
+</a>
+  `;
+};
+
+const galleryCards = galleryItems.map((el, idx, arr) => {
+  return makeGalleryCard(el);
+});
+
+galleryList.insertAdjacentHTML("afterbegin", galleryCards.join(""));
+
+new SimpleLightbox('.gallery a', { 
+    captionsData: 'alt',
+    captionPosition: 'bottom',
+    captionDelay: 250, 
+});
