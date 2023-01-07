3064455fa4f5c0d38a4cd0903045614d5a56868d
Anna Atiskova <anna.atiskova@gmail.com>
2023-01-07 21:58:44 +0200

    task2

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,12 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
+const iframe = document.querySelector('iframe');
+const player = new Player(iframe);
+
+const onPlay = e => localStorage.setItem('videoplayer-current-time', e.seconds);
+const currentTime = localStorage.getItem('videoplayer-current-time');
+
+if (currentTime) player.setCurrentTime(currentTime);
+
+player.on('timeupdate', throttle(onPlay, 1000));
