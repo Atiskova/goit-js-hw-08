43e1ec1b91f10826b8c250ea2a5e21ae7c13c7f5
Anna Atiskova <anna.atiskova@gmail.com>
2023-01-07 02:42:25 +0200

    task3

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,44 @@
+import throttle from 'lodash.throttle';
 
+const contactFormEl = document.querySelector('.feedback-form');
+const userInfo = {};
+
+
+const fillContactFormFields = () => {
+    try {
+        const userInfoFormLS = JSON.parse(localStorage.getItem('feedback-form-state'));
+        if (userInfoFormLS === null) {
+        return
+        }
+        console.log(userInfoFormLS);
+
+        for (const prop in userInfoFormLS) {
+        contactFormEl.elements[prop].value = userInfoFormLS[prop];
+        }
+    } catch(err) {
+        console.log(err);
+    }
+};  
+
+fillContactFormFields();
+
+const onContactFormFieldInput = event => {
+const { target } = event;
+
+// const fieldValue = target.value;
+// const fieldName = target.name;
+
+userInfo[target.name] = target.value;
+
+
+localStorage.setItem('feedback-form-state', JSON.stringify(userInfo));
+};
+
+const onContactFormFieldSubmit = event => {
+    event.preventDefault();
+    contactFormEl.reset();
+    localStorage.removeItem('feedback-form-state');
+};
+
+contactFormEl.addEventListener('input', throttle(onContactFormFieldInput, 500));
+contactFormEl.addEventListener('submit', onContactFormFieldSubmit);
